1be86da331e22d3baa3ef6a5b63d3c79fb78bb5b
4rca31 <da.esfra12@gmail.com>
2024-06-30 19:51:55 -0600

    Add office script install

---
+++ arch/utils/office.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Install libre office in linux
+# By: 4rcan31
+#
+# Requirements: Have an Arch Linux-based distribution installed
+#
+
+
+sudo pacman -S libreoffice-still --noconfirm
