a23871ac20a542abce0db37eda3d52c7c1ec819d
4rca31 <da.esfra12@gmail.com>
2024-06-30 19:52:40 -0600

    Add program to custumizate gnome DE

---
+++ arch/Desktop-environments/gnome/gnome-tweaks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Install gnome tweaks for customization gnome interface 
+# By: 4rcan31
+#
+# Requirements: Have an Arch Linux-based distribution installed
+#
+
+yay -S gnome-tweaks
