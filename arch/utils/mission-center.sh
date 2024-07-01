e27a258f8dd2a21e8eb5f20f0945766a3c76c4c8
4rca31 <da.esfra12@gmail.com>
2024-06-30 19:51:34 -0600

    Add script in utils for install mission center, it is a program similar to task manager windows

---
+++ arch/utils/mission-center.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Install mission-center similar to task manager in windows
+# By: 4rcan31
+#
+# Requirements: Have an Arch Linux-based distribution installed
+
+yay -S mission-center
