671212f8bb4da7208ff4518d36b866657ef0b44b
4rca31 <da.esfra12@gmail.com>
2024-06-30 20:24:45 -0600

    Add file to install connector for install extensions from the browser

---
+++ arch/Desktop-environments/gnome/extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Install connector to can install extensions from browser in this url: https://extensions.gnome.org/
+# also you have install extention in your brownser: https://chromewebstore.google.com/detail/gphhapmejobijbbhgpjhcjognlahblep
+# By: 4rcan31
+#
+# Requirements: Have an Arch Linux-based distribution installed
+#
+
+sudo pacman -Sy gnome-browser-connector --noconfirm
