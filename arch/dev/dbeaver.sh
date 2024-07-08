a616add21859e97d329ce780538c34cf87e63ac9
4rca31 <da.esfra12@gmail.com>
2024-07-07 18:07:17 -0600

    add dbeaver script install

---
+++ arch/dev/dbeaver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Install dbeaver as client universal data base
+# By: 4rcan31
+#
+# Requirements: Have an Arch Linux-based distribution installed
+#
+# Start of configuration
+
+yay -S dbeaver
+
