adf640956bff8fb08417bd867dd79fd2e9d1f9d1
4rca31 <da.esfra12@gmail.com>
2024-07-07 18:07:17 -0600

    add script install sdkman

---
+++ arch/dev/sdkman.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Configuration of environment for java with sdkman
+# By: 4rcan31
+#
+# Requirements: Have an Arch Linux-based distribution installed
+#
+# Start of configuration
+
+
+curl -s "https://get.sdkman.io" | bash
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
